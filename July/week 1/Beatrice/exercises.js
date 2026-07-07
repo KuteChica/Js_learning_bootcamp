0e442e0c5bb92171039b0acca75cdc66820fd3dd
Kpemini Delali Beatrice <kpeminibeatrice18@gmail.com>
2026-07-07 14:59:08 +0000

    added modified notes.md & exercises.js for week 1

---
+++ July/week 1/Beatrice/exercises.js
@@ -0,0 +1,75 @@
+let name = "Beatrice";
+let age = 20;
+let school = "GCTU";
+let isStudent = true;
+
+console.log("My name is " +name)
+console.log("I am " + age + " years old")
+console.log("I school at " + school)
+console.log(isStudent)
+
+// let VS const
+//
+// Ghana can have const
+
+// my age can't be const because it will probably change year after year
+
+// pi can be a const because i have never seen or heard it change before
+
+// number of lives in agame should use let because the lives in a game depends on the performance of the player
+
+// today's date should use const because today's date can't be changed it remains const
+
+
+// data type
+//
+// 25- number
+
+//"React" - a string
+
+//true- boolean
+
+// null- null type(nothing in the variable)
+
+// undefined - a variable that hasn't be assigned an expression
+
+
+// operators
+//
+
+// 20+10 = 30
+
+// 20-10 = 10
+
+// 20/10 = 2
+
+// 20 % 6 = 2
+
+
+// == and ===
+//
+
+// 10 == "10" -> true
+
+// 10 === "10" -> false
+
+// true == 1 -> true
+
+// true === 1 -> true
+
+age = age + 5
+
+console.log("I will be " + age + " after 5 years!!!")
+
+
+let favFood = "Kenkey and fish"
+let favProgrammingLanguage = "JavaScript"
+let dreamCompany = "Google"
+
+console.log("My favourite food is " + favFood)
+console.log("My favourite programming language is " + favProgrammingLanguage)
+console.log("My dream company is " + dreamCompany)
+
+console.log("Hello " + name +"!")
+console.log("Next year you will be " + (age + 1) + " years old.")
+console.log("You study at " + school)
