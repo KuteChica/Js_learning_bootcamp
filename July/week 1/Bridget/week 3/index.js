1acffd33841873bfea37653f21150314d3899085
KuteChica <bridgettossou13@gmail.com>
2026-07-28 19:14:25 +0000

    this is a project from scrimba i am trying to work on

---
+++ July/week 1/Bridget/week 3/index.js
@@ -0,0 +1,34 @@
+ 
+    // document.getElementById("count-el").innerText=5;
+
+
+// console.log(count);
+
+// let myage = 35;
+// let seconbatch =7;
+//  console.log(myage)
+
+//  let firstbatch =5
+  
+// let count = firstbatch
+
+// console.log(count)
+
+// let myage = 5;
+// let humanDogRatio =6;
+// let myDogage = myage * humanDogRatio;
+
+// console.log(myDogage)
+// let bonuspoints = 50;
+// bonuspoints = bonuspoints + 50;
+// console.log(bonuspoints);
+
+// bonuspoints = bonuspoints-25
+// ;
+// console.log(bonuspoints);
+//  bonuspoints = bonuspoints +20;
+//  console.log(bonuspoints)
+
+function increment(){
+  console.log("the button was clicked")
+}
